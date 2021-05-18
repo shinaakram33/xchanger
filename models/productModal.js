8bdd81fbe62f8f6b5b297fe8e9b54fc777503383
Fahad <fahadtufail873@gmail.com>
2021-05-18 15:47:43 +0500

    fix state into country

--- models/productModal.js
+++ models/productModal.js
@@ -37,7 +37,7 @@ const productSchema = new mongoose.Schema(
       type: String,
       required: [true, 'Category Options Name is required'],
     },
-    state: {
+    country: {
       type: String,
       required: [true, 'State of a product is required'],
       enum: {
