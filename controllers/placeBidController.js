08b292449cca3af1cfa8d2978717a5aa09af5dcd
muqaddasAshraf <>
2022-04-24 00:57:54 +0500

    Cron job removed

--- controllers/placeBidController.js
+++ controllers/placeBidController.js
@@ -37,19 +37,19 @@ exports.createplaceBid = async (req, res) => {
         });
       }
           
-      // const token = await stripe.tokens.create({
-      //     card: {
-      //       number: "4242424242424242",
-      //       exp_month: 1,
-      //       exp_year: 2023,
-      //       cvc: "314",
-      //     },
-      //   });
+      const token = await stripe.tokens.create({
+          card: {
+            number: "4242424242424242",
+            exp_month: 1,
+            exp_year: 2023,
+            cvc: "314",
+          },
+        });
         const paymentMethod = await stripe.paymentMethods.create({
           type: "card",
           card: {
-            token: req.body.source,
-            // token: token.id
+            // token: req.body.source,
+            token: token.id
           },
         });
     
@@ -86,7 +86,7 @@ exports.createplaceBid = async (req, res) => {
           text: `New bid placed on your product ${product.title}`,
         };
 
-        fetch("https://x-changer.herokuapp.com/api/v1/notification/notification", {
+        fetch("https://x-changer.herokuapp.com/api/v1/notification", {
           method: "POST",
           body: JSON.stringify(data),
           headers: { "Content-Type": "application/json" },
@@ -105,166 +105,8 @@ exports.createplaceBid = async (req, res) => {
             console.log(error);
           });
         
-        console.log(product.date_for_auction);
   
-        let min = moment(product.date_for_auction.ending_date).minutes();
-        let hour = moment(product.date_for_auction.ending_date).hours();
-        let day = moment(product.date_for_auction.ending_date).format('D');
-        let month = moment(product.date_for_auction.ending_date).format('M');
-        let year = moment(product.date_for_auction.ending_date).format('Y');
-  
-        console.log(min, hour, day, month, year)
         
-  
-        let paymentIntentCaptureJob = schedule.scheduleJob(`${min} ${hour} ${day} ${month} *`, async () => {
-          console.log('Cron job executed.')
-          
-          const order = await Order.create({
-            name: req.body.name,
-            email: req.body.email.trim().toLowerCase(),
-            phoneNumber: req.body.phoneNumber,
-            location: req.body.location,
-            user: req.user.id,
-            checkoutId: placeBid.intentId,
-            status: "Complete",
-            price: req.body.price,
-            productId: product.id,
-            shippingFee: req.body.shippingFee,
-          }).then(o => o.populate("productId").execPopulate());
-          console.log("Order", order);
-          product.status = 'Sold';
-          await product.save();
-          console.log(product);
-
-          //find all bids on the product
-          let allBidsOfProduct = await placebid.find({product: product.id})
-          .sort({price: -1, createdAt: -1});
-          console.log(allBidsOfProduct.length, allBidsOfProduct);
-
-          
-          //inform user about purchase
-          let highestBid = allBidsOfProduct.shift();
-          console.log('highest', highestBid);
-
-          highestBid.succeeded = true;
-          
-          const paymentIntentCapture = await stripe.paymentIntents.capture(
-            highestBid.intentId
-          );
-          let userData = {
-            user: highestBid.user,
-            product: product.id,
-            text: `Your bid on product ${product.title} has been successful.`,
-          };
-
-          fetch("https://x-changer.herokuapp.com/api/v1/notification/notification", {
-            method: "POST",
-            body: JSON.stringify(userData),
-            headers: { "Content-Type": "application/json" },
-          })
-            .then(async (res) => {
-              try {
-                const dataa = await res.json();
-                console.log("response data?", dataa);
-              } catch (err) {
-                console.log("error");
-                console.log(err);
-              }
-            })
-            // .then((json) => console.log("json ", json))
-            .catch((error) => {
-              console.log(error);
-            });
-          console.log(paymentIntentCapture);
-          if (paymentIntentCapture.status === "succeeded") {
-  
-            console.log("status ", paymentIntentCapture.status);
-                          
-            //make payment to seller
-            const productSeller = await User.findById(product.user);
-            console.log(productSeller);
-
-            // try{
-              //     let transfer = await stripe.transfers.create({
-              //     amount: Math.round(highestBid * 100),
-            //       currency: 'usd',
-            //       destination: productSeller.connAccount.id,
-            //       source_transaction: paymentIntentCapture.charges.data[0].id,
-            //     });
-            //     console.log(transfer);
-            //   } catch (err) {
-            //     console.log(err);
-            //   }
-  
-            let sellerData = {
-              user: product.user,
-              product: product.id,
-              text: `Your  product ${product.title} has been sold`,
-            };
-
-            fetch("https://x-changer.herokuapp.com/api/v1/notification/notification", {
-              method: "POST",
-              body: JSON.stringify(sellerData),
-              headers: { "Content-Type": "application/json" },
-            })
-              .then(async (res) => {
-                try {
-                  const dataa = await res.json();
-                  console.log("response data?", dataa);
-                } catch (err) {
-                  console.log("error");
-                  console.log(err);
-                }
-              })
-              // .then((json) => console.log("json ", json))
-              .catch((error) => {
-                console.log(error);
-              });
-          } else {
-            return res.status(400).json({
-              status: "fail",
-              message: "Stripe error",
-            });
-          }
-
-
-          //delete other bids
-          allBidsOfProduct.forEach(async (bid) => {
-            await stripe.paymentIntents.cancel(bid.intentId);
-            await placebid.deleteOne({_id: bid.id});
-          });
-          
-          //send notification to other users
-          let failedBidUsers = await placebid.distinct('user', {"product": product.id});
-          console.log('failedBidUsers', failedBidUsers);
-          failedBidUsers.forEach((user) => {
-            let data = {
-              user: user,
-              product: product.id,
-              text: `Your bid on product ${product.title} failed. The product has been sold`,
-            };
-
-            fetch("https://x-changer.herokuapp.com/api/v1/notification/notification", {
-              method: "POST",
-              body: JSON.stringify(data),
-              headers: { "Content-Type": "application/json" },
-            })
-              .then(async (res) => {
-                try {
-                  const dataa = await res.json();
-                  console.log("response data?", dataa);
-                } catch (err) {
-                  console.log("error");
-                  console.log(err);
-                }
-              })
-              // .then((json) => console.log("json ", json))
-              .catch((error) => {
-                console.log(error);
-              });
-          })
-
-        });
         return res.status(201).json({
           status: 'success',
           message: 'Bidding is created successfully',
@@ -276,7 +118,7 @@ exports.createplaceBid = async (req, res) => {
           message: "Stripe error",
         });
       }
-    } 
+    }
   } catch (err) {
     return res.status(400).json({
       status: 'fail',
